04f65dadd7ebe434457248ff50763c1f6e08ef8f
LilLongg <glivingstone70@gmail.com>
2024-03-10 23:55:07 +0700

    Update moving objects can move in multiple directions

--- Level4/Main.py
+++ Level4/Main.py
@@ -1,5 +1,8 @@
 from Graph import Graph
 
 
-graph = Graph("input.txt")
-graph.run()
+try:
+    graph = Graph(input("Enter the name of the input file: "))
+    graph.run()
+except Exception as e:
+    print(repr(e))
